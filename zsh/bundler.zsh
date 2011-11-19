02cb4916f3c856691682cf123c625d8878955ef8
Wynn Netherland <wynn.netherland@gmail.com>
2011-11-19 11:49:43 -0600

    New aliases, vim plugins

---
+++ zsh/bundler.zsh
@@ -0,0 +1,6 @@
+alias b="bundle"
+alias bi="b install --path vendor"
+alias bil="bi --local"
+alias bu="b update"
+alias be="b exec"
+alias binit="bi && b package && echo 'vendor/ruby' >> .gitignore"
--- zsh/common.zsh
+++ zsh/common.zsh
@@ -1,4 +1,4 @@
-
+alias cls="clear"
 alias vim="mvim -v"
 alias lastfm="shell-fm"
 
