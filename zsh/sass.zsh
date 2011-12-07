78900704cc2ae703bfe305eadeea8ffa41f6b7c1
Wynn Netherland <wynn.netherland@gmail.com>
2011-12-07 10:25:07 -0600

    Smack a folder for smacss

---
+++ zsh/sass.zsh
@@ -0,0 +1,17 @@
+function smack {
+  if [[ -z $1 ]]; then
+    SMACK_FOLDER=.;
+  else
+    SMACK_FOLDER=$1;
+  fi
+
+  mkdir -p $SMACK_FOLDER/base;
+  mkdir -p $SMACK_FOLDER/layout;
+  mkdir -p $SMACK_FOLDER/modules;
+  mkdir -p $SMACK_FOLDER/state;
+
+  touch $SMACK_FOLDER/_base.sass;
+  touch $SMACK_FOLDER/_layout.sass;
+  touch $SMACK_FOLDER/_modules.sass;
+  touch $SMACK_FOLDER/_state.sass;
+}
