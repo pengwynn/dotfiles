c32be12e54d3a8c72d7557f4d7e8cd8bc24d7a99
Wynn Netherland <wynn.netherland@gmail.com>
2012-03-23 22:41:49 -0500

    Updates alias for titanium compilation

--- zsh/titanium.zsh
+++ zsh/titanium.zsh
@@ -1,9 +1,19 @@
 # Titanium stuff
 
 export ANDROID_SDK="/Users/wynn/android_sdk"
-
-alias titanium='/Library/Application\ Support/Titanium/mobilesdk/osx/1.7.2/titanium.py'
-alias coffee-titanium='coffee -c --bare -o Resources --watch src'
 alias adb='/Users/wynn/android_sdk/platform-tools/adb'
+
+
+alias titan='~/Library/Application\ Support/Titanium/mobilesdk/osx/1.8.2/titanium.py'
+alias tic='coffee -c --bare -o Resources src'
+alias tib='titan run --platform=iphone'
+
+function tss() {
+  rm Resources/app.jss;
+  sass --compass -C -t expanded sass/app.sass > Resources/app.jss;
+}
+
+alias ticl='rm -rf build/iphone'
+
 
 
