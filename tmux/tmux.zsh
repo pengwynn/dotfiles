ae51abf6d4ea71ac71e2bc26236d7de0d6ceddf0
Wynn Netherland <wynn.netherland@gmail.com>
2012-04-30 14:50:58 -0500

    Rolling my own tmux layouts, keeping up with @adamyonk

--- tmux/tmux.zsh
+++ tmux/tmux.zsh
@@ -1,23 +1,7 @@
-function tmxs() {
-  SOCKET_PATH=$1
-  chmod 777 /tmp/tmux-$SOCKET_PATH
+function mx() {
+  if [[ -x $DOTFILES/tmux/layouts/$1 ]]; then
+    $DOTFILES/tmux/layouts/$1
+  else
+    tmux new-session -s $1
+  fi
 }
-
-function colours() {
-  for i in {0..255} ; do
-    printf "\x1b[38;5;${i}mcolour${i}\n"
-  done
-}
-
-function tpbcopy {
-  tmux showb > /tmp/tmxcp
-}
-
-function tpbpaste {
-  cat /tmp/tmxcp | pbcopy
-}
-
-function tmw {
-  tmux split-window -dh "$*"
-}
-
--- zsh/common.zsh
+++ zsh/common.zsh
@@ -4,4 +4,9 @@ export EDITOR=vim
 alias cls="clear"
 alias lastfm="shell-fm"
 
+function colours() {
+for i in {0..255} ; do
+  printf "\x1b[38;5;${i}mcolour${i}\n"
+done
+}
 
