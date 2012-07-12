0036da9c85917203d6030366ede93a090612b204
Wynn Netherland <wynn@github.com>
2012-07-12 11:11:42 -0700

    Smarter mx script

--- tmux/tmux.zsh
+++ tmux/tmux.zsh
@@ -2,6 +2,10 @@ function mx() {
   if [[ -x $DOTFILES/tmux/layouts/$1 ]]; then
     $DOTFILES/tmux/layouts/$1
   else
-    tmux new-session -s $1
+    if ! tmux has-session -t $1; then
+      tmux new-session -s $1 -n zsh -d
+    fi
+
+    tmux attach -t $1
   fi
 }
