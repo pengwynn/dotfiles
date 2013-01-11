19f629ad5178adcc2f534c158ededdbf96222a5d
Wynn Netherland <wynn@github.com>
2013-01-10 21:23:51 -0600

    Clean up old Ruby aliases

--- ruby/bundler.zsh
+++
@@ -1,6 +0,0 @@
-alias b="bundle"
-#alias bi="b install --path vendor"
-#alias bil="bi --local"
-alias bu="b update"
-alias be="b exec"
-alias binit="bi && b package && echo 'vendor/ruby' >> .gitignore"
