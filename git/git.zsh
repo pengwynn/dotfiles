c4ecc803ed104b7fe01096c087495acfd775a7e7
Wynn Netherland <wynn@github.com>
2013-03-10 17:50:50 -0500

    Prune unused git-related shell aliases

--- git/git.zsh
+++ git/git.zsh
@@ -13,29 +13,7 @@ function g {
     fi
 }
 
-# Git flow
-alias gff='git flow feature'
-alias gffs='git flow feature start'
-alias gfff='git flow feature finish'
-alias gfr='git flow release'
-alias gfrs='git flow release start'
-alias gfrf='git flow release finish'
-alias gfh='git flow hotfix'
-alias gfhs='git flow hotfix start'
-alias gfhf='git flow hotfix finish'
-alias gfs='git flow support'
-alias gfss='git flow support start'
-
 alias gst='git status -sb'
-
-# Pairing
-hitch() {
-  command hitch "$@"
-  if [[ -s "$HOME/.hitch_export_authors" ]] ; then source "$HOME/.hitch_export_authors" ; fi
-}
-alias unhitch='hitch -u'
-# Uncomment to persist pair info between terminal instances
-# hitch
 
 function minutes_since_last_commit {
     now=`date +%s`
