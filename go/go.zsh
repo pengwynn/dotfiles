bf385058fcbd847eeb07335907a08f3d6a708264
Wynn Netherland <wynn@github.com>
2013-09-02 10:21:12 -0500

    go pack

--- go/go.zsh
+++ go/go.zsh
@@ -1,3 +1,4 @@
-export GOROOT="/opt/boxen/homebrew/Cellar/go/1.1.1"
+export GOROOT="/opt/boxen/homebrew/Cellar/go/1.1.2"
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
+export GOPACK_SKIP_COLORS=1
