9e18ae2084e9281d613612444b582a7d45be9958
Wynn Netherland <wynn@github.com>
2014-01-21 11:23:13 -0600

    Upgrade to Go 1.2

--- go/go.zsh
+++ go/go.zsh
@@ -1,4 +1,3 @@
-export GOROOT="/opt/boxen/homebrew/Cellar/go/1.1.2"
+# export GOROOT="/opt/boxen/homebrew/Cellar/go/1.2"
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
-export GOPACK_SKIP_COLORS=1
