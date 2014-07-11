e568b67f1cf48abef2a4eca9337557ada5df75cd
Wynn Netherland <wynn@github.com>
2014-07-11 08:50:06 -0500

    Bump XCode selector for Beta3

--- osx/xcode.zsh
+++ osx/xcode.zsh
@@ -1,2 +1,2 @@
 alias use_xcode5="sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer"
-alias use_xcode6="sudo xcode-select -switch /Applications/Xcode6-Beta.app/Contents/Developer"
+alias use_xcode6="sudo xcode-select -switch /Applications/Xcode6-Beta3.app/Contents/Developer"
