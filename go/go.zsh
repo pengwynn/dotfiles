9f117b8ca8693fa63684640624cf43563d985795
Wynn Netherland <wynn@github.com>
2014-08-09 12:45:37 -0500

    gcd function to jump to projects in GOPATH/src

--- go/go.zsh
+++ go/go.zsh
@@ -1,3 +1,9 @@
 # export GOROOT="/opt/boxen/homebrew/Cellar/go/1.2"
 export GOPATH=$HOME/go
+export GOSRC=$GOPATH/src/github.com
 export PATH=$PATH:$GOPATH/bin
+
+
+function gcd {
+  cd "$GOSRC/$1"
+}
