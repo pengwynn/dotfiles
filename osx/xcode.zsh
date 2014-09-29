b61e12cf1a8526e9798920ca3b43ad5a29b9de74
Wynn Netherland <wynn@github.com>
2014-09-28 21:14:23 -0500

    Remove xcode selection aliases

--- osx/xcode.zsh
+++
@@ -1,2 +0,0 @@
-alias use_xcode5="sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer"
-alias use_xcode6="sudo xcode-select -switch /Applications/Xcode6-Beta5.app/Contents/Developer"
