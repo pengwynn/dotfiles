40a21bdab3ebaab77f4e72ea00507710b57fd754
Wynn Netherland <wynn@github.com>
2015-05-09 18:15:49 -0500

    Tweak Go colors for grc

--- color/grc.symlink/conf.go
+++ color/grc.symlink/conf.go
@@ -1,10 +1,20 @@
 
+# RUN
+regexp==== RUN (.*)
+colours=black
+count=more
+===
 # PASS
-regexp=PASS
+regexp=(.*)PASS:?(.*)
 colours=green
 count=more
 =====
 # FAIL
-regexp=FAIL
+regexp=(.*)FAIL:?(.*)
 colours=red
+count=more
+=====
+# SKIP
+regexp=(.*)SKIP:?(.*)
+colours=yellow
 count=more
