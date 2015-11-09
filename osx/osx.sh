052a969554f64043b440819075518f414133b9cb
Wynn Netherland <wynn@github.com>
2015-11-08 22:04:01 -0600

    Key repeat

--- osx/osx.sh
+++ osx/osx.sh
@@ -74,7 +74,7 @@ defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
 # Disable press-and-hold for keys in favor of key repeat
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool true
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -float 0.01
