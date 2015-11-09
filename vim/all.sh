43e074561ab0643e67abf5136c98c2a8f34552d3
Wynn Netherland <wynn@github.com>
2015-11-08 22:12:31 -0600

    Try vim-plug

--- vim/all.sh
+++ vim/all.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim +PlugInstall +qa
+
