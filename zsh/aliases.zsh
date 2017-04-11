67774200cae43c5c151504738020a123416f6d34
Wynn Netherland <wynn.netherland@gmail.com>
2017-04-11 09:10:53 -0500

    Update interface for getting local IP

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,7 +7,7 @@ alias shout="tr '[[:lower:]]' '[[:upper:]]'"
 
 # IP addresses
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias localip="ipconfig getifaddr en1"
+alias localip="ipconfig getifaddr en0"
 alias ips="ifconfig -a | grep -o 'inet6\? \(\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\|[a-fA-F0-9:]\+\)' | sed -e 's/inet6* //'"
 
 # Enhanced WHOIS lookups
