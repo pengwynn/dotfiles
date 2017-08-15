a3e69cd7c1d87bd84d499b4c42e3056a7b525633
Wynn Netherland <wynn.netherland@gmail.com>
2017-08-15 21:46:55 +0000

    Add golang to asdf

--- versioning/plugins.sh
+++ versioning/plugins.sh
@@ -1,6 +1,6 @@
 #/bin/env bash
 
 set -e
-
+asdf plugin-add golang https://github.com/kennyp/asdf-golang
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
