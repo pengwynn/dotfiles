2aafbb98980730778e3b2ed668e7fbebeb493779
Wynn Netherland <wynn.netherland@gmail.com>
2019-04-06 10:36:47 -0500

    Add Codi shortcut

--- zsh/common.zsh
+++ zsh/common.zsh
@@ -31,3 +31,17 @@ if [ -n "$TMUX"  ]; then
 else
   function refresh {  }
 fi
+
+# Codi
+# Usage: codi [filetype] [filename]
+codi() {
+  local syntax="${1:-python}"
+  shift
+  vim -c \
+    "let g:startify_disable_at_vimenter = 1 |\
+    set bt=nofile ls=0 noru nonu nornu |\
+    hi ColorColumn ctermbg=NONE |\
+    hi VertSplit ctermbg=NONE |\
+    hi NonText ctermfg=0 |\
+    Codi $syntax" "$@"
+}
